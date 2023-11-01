3783f7ce7533ad56db8b5de6b55f69d49f555efe
mouadse <mouadse@gmail.com>
2023-11-02 00:16:31 +0100

    Added a comment in the file as a test

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 
 int main() {
     // This is a simple comment in C language
+    // This is a simple comment
+    printf("this is a simple test");
     printf("Hello, World!\n");
     return 0;
 }
