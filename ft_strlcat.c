ba3f6c7a1eb88f236fc455c0a37af9a5c8340c90
mouadse <mouadse@gmail.com>
2023-11-04 16:21:03 +0100

    Done

---
+++ ft_strlcat.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mouad <mouad@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/04 16:11:34 by mouad             #+#    #+#             */
+/*   Updated: 2023/11/04 16:12:39 by mouad            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t ft_strlcat(char *dst, const char *src, size_t dstsize) {
+    size_t i;
+    size_t dst_len;
+    size_t src_len;
+
+    i = 0;
+    dst_len = ft_strlen(dst);
+    src_len = ft_strlen(src);
+
+    if (dstsize <= dst_len)
+        return (dst_len + dstsize);
+
+    while (src[i] && (dst_len + i) < (dstsize - 1)) {
+        dst[dst_len + i] = src[i];
+        i++;
+    }
+    dst[dst_len + i] = '\0';
+    return (dst_len + src_len);
+}
