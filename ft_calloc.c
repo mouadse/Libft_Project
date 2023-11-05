6f72ac29b213d95d85c9cb56a294d00732920527
mouadse <mouadse@gmail.com>
2023-11-05 19:31:56 +0100

    Done

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,15 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msennane <msennane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/05 19:29:09 by msennane          #+#    #+#             */
+/*   Updated: 2023/11/05 19:29:10 by msennane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	char *ptr;
-	size_t i;
+	char	*ptr;
+	size_t	i;
 
 	i = -1;
 	ptr = malloc(count * size);
 	if (!ptr)
 		return (NULL);
-
 	while (++i < count * size)
 		ptr[i] = 0;
 	return (ptr);
