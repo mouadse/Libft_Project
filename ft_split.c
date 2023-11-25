79e017f61d76d2c37d2b1e51d6c1ee8eb70a4784
mouadse <mouadse@gmail.com>
2023-11-25 17:38:08 +0100

    Done

--- ft_split.c
+++ ft_split.c
@@ -6,31 +6,81 @@
 /*   By: msennane <msennane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 23:42:07 by msennane          #+#    #+#             */
-/*   Updated: 2023/11/23 21:17:40 by msennane         ###   ########.fr       */
+/*   Updated: 2023/11/25 17:37:46 by msennane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_count_words(char const *s, char c)
+static size_t	ft_countword(char const *s, char c)
 {
-	int	i;
-	int	count;
+	size_t	i;
+	size_t	old_i;
+	size_t	count;
 
 	i = 0;
+	old_i = 0;
 	count = 0;
 	while (s[i])
 	{
-		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
+		while (s[i] && s[i] == c)
+			i++;
+		old_i = i;
+		while (s[i] && s[i] != c)
+			i++;
+		if (old_i < i)
 			count++;
-		i++;
 	}
 	return (count);
 }
 
+static size_t	compute_w_len(char const *s, char c)
+{
+	size_t	len;
+
+	len = 0;
+	while (*s && *s == c)
+		s++;
+	while (s[len] && s[len] != c)
+		len++;
+	return (len);
+}
+
+static char	**free_strs(char **strs, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	while (i <= size)
+		free(strs[i++]);
+	free(strs);
+	return (NULL);
+}
+
 char	**ft_split(char const *s, char c)
 {
-	char	**result;
+	char	**split;
+	size_t	words_count;
+	size_t	i;
+	size_t	cpd_w;
 
-	return (result);
+	if (!s)
+		return (NULL);
+	i = 0;
+	cpd_w = 0;
+	words_count = ft_countword(s, c);
+	split = (char **)malloc(sizeof(char *) * (words_count + 1));
+	if (!split)
+		return (NULL);
+	split[words_count] = NULL;
+	while (s[i] && cpd_w < words_count)
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		split[cpd_w] = ft_substr(s, i, compute_w_len(s + i, c));
+		if (!split[cpd_w++])
+			return (free_strs(split, words_count));
+		i += compute_w_len(s + i, c);
+	}
+	return (split);
 }
