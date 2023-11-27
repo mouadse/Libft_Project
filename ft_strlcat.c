bbc631e8618d103ca794f7b11e30b9ce830f765c
mouadse <mouadse@gmail.com>
2023-11-27 01:06:02 +0100

    Fixed ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: msennane <msennane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 16:11:34 by mouad             #+#    #+#             */
-/*   Updated: 2023/11/27 00:44:37 by msennane         ###   ########.fr       */
+/*   Updated: 2023/11/27 01:05:34 by msennane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	i = 0;
 	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
-	if (dstsize <= dst_len)
+	if (dstsize <= dst_len || dstsize == 0)
 		return (dst_len + dstsize);
 	while (src[i] && (dst_len + i) < (dstsize - 1))
 	{
