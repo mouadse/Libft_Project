f8d0296b9b9dc64e541ef1ebc6eb68070ea61af9
mouadse <mouadse@gmail.com>
2023-11-27 01:23:24 +0100

    Done

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: msennane <msennane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 16:11:34 by mouad             #+#    #+#             */
-/*   Updated: 2023/11/27 01:05:34 by msennane         ###   ########.fr       */
+/*   Updated: 2023/11/27 01:22:56 by msennane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,18 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t	i;
 	size_t	dst_len;
-	size_t	src_len;
+	size_t	i;
 
-	i = 0;
 	dst_len = ft_strlen(dst);
-	src_len = ft_strlen(src);
+	i = 0;
 	if (dstsize <= dst_len || dstsize == 0)
-		return (dst_len + dstsize);
-	while (src[i] && (dst_len + i) < (dstsize - 1))
+		return (dstsize + ft_strlen(src));
+	while (src[i] && (dst_len + i) < dstsize - 1)
 	{
 		dst[dst_len + i] = src[i];
 		i++;
 	}
 	dst[dst_len + i] = '\0';
-	return (dst_len + src_len);
+	return (dst_len + ft_strlen(src));
 }
