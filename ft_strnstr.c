c404d68793683bc097b781dbc4340c965021fd9b
mouadse <mouadse@gmail.com>
2023-11-27 01:30:46 +0100

    Done

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: msennane <msennane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 18:03:44 by msennane          #+#    #+#             */
-/*   Updated: 2023/11/05 18:14:42 by msennane         ###   ########.fr       */
+/*   Updated: 2023/11/27 01:30:01 by msennane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,21 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	haystack_len;
-	size_t	needle_len;
 	size_t	i;
 	size_t	j;
 
-	haystack_len = ft_strlen(haystack);
-	needle_len = ft_strlen(needle);
-	if (needle_len == 0)
-		return ((char *)haystack);
 	i = 0;
-	while ((i < len) && (i < haystack_len))
+	if (needle[i] == '\0')
+		return ((char *)haystack);
+	while (haystack[i] && i < len)
 	{
 		j = 0;
-		while ((j < needle_len) && (haystack[i + j] == needle[j]))
+		while (haystack[i + j] == needle[j] && (i + j) < len)
+		{
+			if (needle[j + 1] == '\0')
+				return ((char *)haystack + i);
 			j++;
-		if (j == needle_len)
-			return ((char *)haystack + i);
+		}
 		i++;
 	}
 	return (NULL);
